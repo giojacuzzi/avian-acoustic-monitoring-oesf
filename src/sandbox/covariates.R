40c5aa818ba902a70502b14a6a67f9898ed6221c
Gio Jacuzzi <giojacuzzi@gmail.com>
2025-06-12 18:22:27 -0700

    update covariates

--- src/sandbox/covariates.R
+++ src/sandbox/covariates.R
@@ -1,40 +1,69 @@
 library(tidyverse)
 library(sf)
 library(terra)
-library(viridis)
 library(mapview)
 options(mapview.maxpixels = 9000000)
+library(ggrepel)
+theme_set(theme_minimal())
+
+# TODO: GET RASTER DATED FOR 2020!
+
+## Helper functions
+
+# Load a raster, cropped, projected, and masked to the study area
+load_raster = function(path_rast) {
+  r = rast(path_rast)
+  r_crop = crop(r, vect(st_transform(study_area, crs(r))))
+  r_proj = project(r_crop, crs(study_area))
+  r_mask = mask(r_proj, vect(study_area))
+  return(r_mask)
+}
+
+# Computer raster function value for a buffer
+computer_raster_buffer_value_func = function(raster, points, buffer_distance, func) {
+  r = project(raster, crs(points))
+  site_buffers = st_buffer(points, dist = buffer_distance)
+  buffer_values = terra::extract(r, vect(site_buffers), fun = func, na.rm = TRUE)
+}
 
 ### Study area
 
-crs_data = 4326 # EPSG:4326
-crs_projected = 32610 # EPSG:32610 (for buffering in meter units)
+crs_m = 32610 # EPSG:32610, UTM Zone 10N, (meters)
 
 # ARU locations
-site_data = gpx::read_gpx('data/sites/PAMlocations20230510.gpx')$waypoints %>%
-  janitor::clean_names() %>% rename(site = name) %>% select(site, latitude, longitude)
-site_data = st_transform(st_as_sf(site_data, coords = c('longitude', 'latitude'), crs = crs_data), crs = crs_projected)
+aru_sites = st_read('data/environment/GIS Data/AcousticStations.shp') %>%
+  st_drop_geometry() %>% janitor::clean_names() %>% as.data.frame() %>%
+  st_as_sf(coords = c("utm_e", "utm_n"), crs = crs_m) %>%
+  select(name, ces, treatment, geometry) %>% rename(site = name)
+mapview(aru_sites, label = aru_sites$name)
+
+site_data = aru_sites
+
+# site_data = gpx::read_gpx('data/sites/PAMlocations20230510.gpx')$waypoints %>%
+#   janitor::clean_names() %>% rename(site = name) %>% select(site, latitude, longitude)
+# site_data = st_transform(st_as_sf(site_data, coords = c('longitude', 'latitude'), crs = crs_data), crs = crs_projected)
 
 # Study area bounding buffer
-study_area = st_buffer(st_as_sfc(st_bbox(site_data)), dist = 100)
+study_area = st_buffer(st_as_sfc(st_bbox(aru_sites)), dist = 100)
 
 # Roads
-roads = st_zm(st_read('data/environment/GIS Data/T3roads.shp'), drop = TRUE)
+roads = st_read('data/environment/GIS Data/T3roads.shp') %>%
+  st_zm(drop = TRUE) %>% janitor::clean_names() %>% st_transform(crs = crs_m)
 
 # Strata and forest inventory units
 
   # Seral stage class
   seral_classes = c("initiation", "canclose", "stemex", "mature")
-  poly_seral_class = st_transform(st_read('data/environment/GIS Data/Forest Development Strata/AgeStrataFixedDIS_RSFRIS20200130.shp'), crs = crs_projected) %>% janitor::clean_names() %>%
-    mutate(class_seral = stratum %>% str_to_lower())
-  poly_seral_class = poly_seral_class %>%
+  class_colors = viridis::viridis(4, option = 'D', direction = -1)
+  
+  poly_seral_class = st_read('data/environment/GIS Data/Forest Development Strata/AgeStrataFixedDIS_RSFRIS20200130.shp') %>%
+    st_transform(crs_m) %>% janitor::clean_names() %>% mutate(class_seral = stratum %>% str_to_lower()) %>%
     mutate(stratum = factor(stratum, levels = seral_classes)) %>% select(-stratum)
   # rsfris_units = st_set_crs(st_read('data/environment/GIS Data/Forest Development Strata/AgeStrataFixedRSFRIS20200130.shp'), 'NAD83')
   
   # Thinning treatment
-  poly_thinning_treatment = st_transform(st_read('data/environment/GIS Data/Forest Development Strata/ThinAfter94NoHarvSinceClipByInitBuf3.shp'), crs = crs_projected) %>%
-    select(TECHNIQUE_, FMA_DT, FMA_STATUS) %>%
-    janitor::clean_names() %>%
+  poly_thinning_treatment = st_read('data/environment/GIS Data/Forest Development Strata/ThinAfter94NoHarvSinceClipByInitBuf3.shp') %>% 
+    st_transform(crs_m) %>% select(TECHNIQUE_, FMA_DT, FMA_STATUS) %>% janitor::clean_names() %>%
     mutate(technique = technique %>% str_to_lower(), fma_status = fma_status %>% str_to_lower()) %>%
     rename(thinning_treatment = technique, thinning_status = fma_status, thinning_date = fma_dt)
 
@@ -44,13 +73,15 @@ table(site_data$class_seral, useNA = 'ifany')
 site_data = st_join(site_data, poly_thinning_treatment)
 table(site_data$thinning_treatment, useNA = 'ifany')
 
+mapview(study_area, col.regions = "transparent", color = "black", lwd = 2) +
+  mapview(poly_seral_class, col.regions = class_colors) +
+  mapview(site_data, zcol = 'class_seral', col.regions = class_colors, legend = T)
+
 # Forest origin (age)
-rast_origin = rast('data/environment/rs_fris/rs_fris_Origin_Year/RS_FRIS_ORIGIN_YEAR.tif') # TODO: GET RASTER DATED FOR 2020!
-crop_vector = vect(st_transform(study_area, crs(rast_origin)))
-rast_origin = mask(crop(rast_origin, crop_vector), crop_vector)
+rast_origin = load_raster('data/environment/rs_fris/rs_fris_Origin_Year/RS_FRIS_ORIGIN_YEAR.tif')
 sort(unique(terra::values(rast_origin)))
 
-rast_age = 2020 - rast_origin
+rast_age = 2024 - rast_origin
 
 # Classify origin raster based on number of years since 2020
 age_classes = c("early", "mid", "late", "old-growth")
@@ -66,55 +97,174 @@ unique(terra::values(rast_age_class))
 levels(rast_age_class) = data.frame(value = 1:4, class_age = age_classes)
 
 # Classify sites based on most abundant origin class within a 100m buffer
-site_buffers = st_buffer(st_transform(site_data, crs(rast_age_class)), dist = 100)
-surrounding_classes = terra::extract(rast_age_class, vect(site_buffers), fun = modal, na.rm = TRUE)
-site_data$class_age = as.factor(surrounding_classes[, 2])
+site_data$class_age = as.factor(computer_raster_buffer_value_func(rast_age_class, site_data, 100, modal)[,2])
 table(as.numeric(site_data$class_age))
 
 # Get mean age
-site_buffers = st_buffer(st_transform(site_data, crs(rast_age)), dist = 100)
-surrounding_classes = terra::extract(rast_age, vect(site_buffers), fun = modal, na.rm = TRUE)
-site_data$age = surrounding_classes[, 2]
-table(as.numeric(site_data$age))
-
-class_colors = viridis(4, option = 'D', direction = -1)
+site_data$age = as.numeric(computer_raster_buffer_value_func(rast_age, site_data, 100, modal)[,2])
+table(site_data$age)
 
 mapview(study_area, col.regions = "transparent", color = "black", lwd = 2) +
   mapview(as.factor(rast_age_class), col.regions = class_colors) +
-  mapview(poly_seral_class, col.regions = class_colors) +
-  mapview(site_data, zcol = 'class_age', col.regions = class_colors, legend = T) +
-  mapview(site_data, zcol = 'class_seral', col.regions = class_colors, legend = T) +
-  mapview(site_data, zcol = 'thinning_treatment', col.regions = class_colors, legend = T)
+  mapview(site_data, zcol = 'class_age', col.regions = class_colors, legend = T)
 
 ### Plot scale variables
 
 # Elevation (m)
 
-  # TODO
+  # Local: TODO
+
+  # Regional: TODO
 
 # Total basal area (m2/ha)
 
-  # Local
+  # Local: TODO
 
   # Regional
-  rast_ba = rast('data/environment/rs_fris/rs_fris_BA/RS_FRIS_BA.tif')
-  crop_vector = vect(st_transform(study_area, crs(rast_ba)))
-  rast_ba = mask(crop(rast_ba, crop_vector), crop_vector)
+  rast_ba = load_raster('data/environment/rs_fris/rs_fris_BA/RS_FRIS_BA.tif')
+  site_data$ba_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_ba, site_data, 100, mean)[,2])
+  site_data$ba_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_ba, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = ba_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = ba_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
   
   mapview(rast_ba) +
-    mapview(site_data, zcol = 'forest_class', col.regions = class_colors, legend = T)
+    mapview(site_data, zcol = 'ba_mean_100m', legend = T)
 
 # Tree height distribution (e.g. SD of height) (m)
+  
+  # Regional
+  rast_ht_lorey = load_raster('data/environment/rs_fris/rs_fris_HT_LOREY/RS_FRIS_HT_LOREY.tif')
+  site_data$ht_lorey_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_ht_lorey, site_data, 100, mean)[,2])
+  site_data$ht_lorey_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_ht_lorey, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = ht_lorey_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = ht_lorey_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_ht_lorey) +
+    mapview(site_data, zcol = 'ht_lorey_mean_100m', legend = T)
+  
+  rast_ht_max = load_raster('data/environment/rs_fris/rs_fris_HTMAX/RS_FRIS_HTMAX.tif')
+  site_data$ht_max_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_ht_max, site_data, 100, mean)[,2])
+  site_data$ht_max_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_ht_max, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = ht_max_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = ht_max_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_ht_lorey) +
+    mapview(site_data, zcol = 'ht_max_mean_100m', legend = T)
 
 # Canopy layers (#)
+  
+  # Local: TODO
+  
+  # Regional
+  rast_canopy_layers = load_raster('data/environment/rs_fris/rs_fris_CANOPY_LAYERS/RS_FRIS_CANOPY_LAYERS.tif')
+  site_data$canopy_layers_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_canopy_layers, site_data, 100, mean)[,2])
+  site_data$canopy_layers_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_canopy_layers, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = canopy_layers_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = canopy_layers_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_canopy_layers) +
+    mapview(site_data, zcol = 'canopy_layers_mean_100m', legend = T)
 
 # Canopy cover
+  
+  # Local: TODO
+  
+  # Regional
+  rast_canopy_cover = load_raster('data/environment/rs_fris/rs_fris_COVER/RS_FRIS_COVER.tif')
+  site_data$canopy_cover_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_canopy_cover, site_data, 100, mean)[,2])
+  site_data$canopy_cover_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_canopy_cover, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = canopy_cover_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = canopy_cover_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_canopy_cover) +
+    mapview(site_data, zcol = 'canopy_cover_mean_100m', legend = T)
+  
+# Canopy closure
+  
+  # Regional
+  rast_canopy_closure = load_raster('data/environment/rs_fris/rs_fris_CLOSURE/RS_FRIS_CLOSURE.tif')
+  site_data$canopy_closure_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_canopy_closure, site_data, 100, mean)[,2])
+  site_data$canopy_closure_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_canopy_closure, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = canopy_closure_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = canopy_closure_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_canopy_closure) +
+    mapview(site_data, zcol = 'canopy_closure_mean_100m', legend = T)
 
 # Number/density of all snags
+  
+  # Local: TODO
+  
+  # Regional
+  rast_snags = load_raster('data/environment/rs_fris/rs_fris_SNAG_ACRE_15/RS_FRIS_SNAG_ACRE_15.tif')
+  site_data$snags_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_snags, site_data, 100, mean)[,2])
+  site_data$snags_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_snags, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = snags_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = snags_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_snags) +
+    mapview(site_data, zcol = 'snags_mean_100m', legend = T)
 
 # Understory vegetation density/cover/volume
+  
+  # Local: TODO
+  
+  # Regional: TODO
+  
+# Dead and downed woody material
+  
+  # Local: TODO
+  
+  # Regional
+  rast_ddwm = load_raster('data/environment/rs_fris/rs_fris_CFVOL_DDWM/RS_FRIS_CFVOL_DDWM.tif')
+  site_data$ddwm_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_ddwm, site_data, 100, mean)[,2])
+  site_data$ddwm_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_ddwm, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = ddwm_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = ddwm_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_ddwm) +
+    mapview(site_data, zcol = 'ddwm_mean_100m', legend = T)
 
 # Tree species composition/richness/diversity
+  
+  # Local: TODO
+  
+  # Regional
+  rast_bap_hwd = load_raster('data/environment/rs_fris/rs_fris_BAP_HWD/RS_FRIS_BAP_HWD.tif')
+  site_data$bap_hwd_mean_100m = as.numeric(computer_raster_buffer_value_func(rast_bap_hwd, site_data, 100, mean)[,2])
+  site_data$bap_hwd_sd_100m = as.numeric(computer_raster_buffer_value_func(rast_bap_hwd, site_data, 100, sd)[,2])
+  
+  ggplot(site_data, aes(x = age, y = bap_hwd_mean_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  ggplot(site_data, aes(x = age, y = bap_hwd_sd_100m, label = site)) +
+    geom_point() + geom_text_repel(size = 2)
+  
+  mapview(rast_bap_hwd) +
+    mapview(site_data, zcol = 'bap_hwd_mean_100m', legend = T)
 
 ### Patch scale variables
 
