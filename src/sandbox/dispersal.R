86103116f6c57d86696b81028521e87c83227097
Gio Jacuzzi <giojacuzzi@gmail.com>
2025-08-04 14:33:21 -0700

    update

--- src/sandbox/dispersal.R
+++ src/sandbox/dispersal.R
@@ -6,23 +6,24 @@ library(ggplot2)
 library(ggrepel)
 theme_set(theme_minimal())
 
-path_species_list = "models/ensemble/ensemble_species_list.txt"
-species_list = data.frame(
-  scientific_name = sort(sapply(strsplit(readLines(path_species_list), "_"), `[`, 1)),
-  common_name = sort(sapply(strsplit(readLines(path_species_list), "_"), `[`, 2))
-)
-
-# https://app.box.com/integrations/officeonline/openOfficeOnline?fileId=1728867025786&sharedAccessCode=
-metadata = read.csv('/Users/giojacuzzi/Downloads/species_metadata(included).csv') %>% select(common_name, scientific_name, home_range_radius_m, residency)
+# Load species metdata - https://app.box.com/integrations/officeonline/openOfficeOnline?fileId=1728867025786&sharedAccessCode=
+metadata = read.csv('data/traits/species_metadata(included).csv', nrows = 107) %>% select(common_name, scientific_name, home_range_radius_m, residency)
 
 avonet = readxl::read_xlsx('data/traits/AVONET Supplementary dataset 1.xlsx', sheet = "AVONET2_eBird") %>%
-  janitor::clean_names() %>% rename(scientific_name = species2, family = family2, order = order2) %>% filter(scientific_name %in% species_list$scientific_name)
+  janitor::clean_names() %>% rename(scientific_name = species2, family = family2, order = order2)
 
-trait_data = left_join(avonet, metadata, by = "scientific_name")
-# Manually resolve common name discrepancies
-trait_data = trait_data %>% mutate(common_name = if_else(scientific_name == "Accipiter gentilis", "Northern Goshawk", common_name))
-trait_data = trait_data %>% mutate(common_name = if_else(scientific_name == "Glaucidium gnoma", "Northern Pygmy-Owl", common_name))
-trait_data = trait_data %>% mutate(common_name = if_else(scientific_name == "Tyto alba", "Barn Owl", common_name))
+# Note that the following subspecies traits are inferred from their superspecies in AVONET:
+# - American Goshawk "Astur atricapillus" (i.e Eurasian/Northern Goshawk "Accipiter gentilis")
+# - Northern Pygmy-owl "Glaucidium californicum" (i.e. Northern/Mountain Pygmy-owl "Glaucidium gnoma")
+# - American Barn Owl "Tyto furcata" (i.e. Western Barn Owl "Tyto alba")
+setdiff(metadata$scientific_name, avonet$scientific_name)
+avonet = avonet %>% mutate(scientific_name = if_else(scientific_name == "Accipiter gentilis", "Astur atricapillus", scientific_name))
+avonet = avonet %>% mutate(scientific_name = if_else(scientific_name == "Glaucidium gnoma", "Glaucidium californicum", scientific_name))
+avonet = avonet %>% mutate(scientific_name = if_else(scientific_name == "Tyto alba", "Tyto furcata", scientific_name))
+setdiff(metadata$scientific_name, avonet$scientific_name)
+
+# Join metadata with AVONET, discarding irrelevant data
+trait_data = left_join(metadata, avonet, by = "scientific_name") %>% select(common_name, scientific_name, family, order, habitat, habitat_density, migration, trophic_level, trophic_niche, primary_lifestyle, residency, hand_wing_index, mass, home_range_radius_m) %>% arrange(common_name)
 
 unknown_home_ranges = trait_data %>% filter(is.na(home_range_radius_m))
 
@@ -76,12 +77,12 @@ ggplot(predicted_home_range_by_mass, aes(x = mass, y = predicted)) +
 
 ###################################################################################
 
-# Fit a generalized linear model relating natural log of hand-wing index to home range radius
-glm_hwi = glm(home_range_radius_m ~ log(hand_wing_index), data = trait_data, family = Gamma(link = "log"))
+# Fit a generalized linear model relating hand-wing index to home range radius
+glm_hwi = glm(home_range_radius_m ~ hand_wing_index, data = trait_data, family = Gamma(link = "log"))
 glm_hwi_pseudoR2 = round(1 - (glm_hwi$deviance / glm_hwi$null.deviance), 2)
 
 # Calculate regression line
-prediction_data = data.frame(hand_wing_index = exp(seq(log(min(trait_data$hand_wing_index)), log(max(trait_data$hand_wing_index)), length.out = 100)))
+prediction_data = data.frame(hand_wing_index = seq(min(trait_data$hand_wing_index), max(trait_data$hand_wing_index), length.out = 100))
 hwi_predictions = predict_with_ci(glm_hwi, prediction_data) %>% mutate(hand_wing_index = prediction_data$hand_wing_index)
 
 ggplot() +
@@ -89,7 +90,7 @@ ggplot() +
   geom_text_repel(data = trait_data, aes(x = (hand_wing_index), y = (home_range_radius_m), label = common_name), size = 3) +
   geom_line(data = hwi_predictions, aes(x = (hand_wing_index), y = (predicted)), color = "blue") +
   geom_ribbon(data = hwi_predictions, aes(x = (hand_wing_index), ymin = (lower), ymax = (upper)), alpha = 0.2, fill = "blue") +
-  scale_x_log10() + scale_y_log10() +
+  scale_x_continuous() + scale_y_log10() +
   labs(
     x = "Hand-wing index", y = "Home range radius (m)",
     title = paste0("Hand-wing index and approximate home range radius (pseudo R2=", glm_hwi_pseudoR2, ")")
@@ -104,7 +105,7 @@ predicted_home_range_by_hwi = predict_with_ci(glm_hwi, prediction_data) %>% muta
 ggplot(predicted_home_range_by_hwi, aes(x = hand_wing_index, y = predicted)) +
   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0) +
   geom_point(size = 2) +
-  scale_x_log10() + scale_y_continuous() +
+  scale_x_continuous() + scale_y_continuous() +
   labs(
     x = "Hand-wing index", y = "Predicted home range radius (m)",
     title = paste0("Home range predictions from hand-wing index")
@@ -112,11 +113,19 @@ ggplot(predicted_home_range_by_hwi, aes(x = hand_wing_index, y = predicted)) +
 
 ###################################################################################
 
-# Fit a generalized linear model relating natural log of body mass and natural log of hand-wing index to home range radius
-glm_combined = glm(home_range_radius_m ~ log(mass) + log(hand_wing_index), data = trait_data, family = Gamma(link = "log"))
+# Fit a generalized linear model relating natural log of body mass and hand-wing index to home range radius
+glm_combined = glm(home_range_radius_m ~ log(mass) + hand_wing_index, data = trait_data, family = Gamma(link = "log"))
 glm_combined_pseudoR2 = round(1 - (glm_combined$deviance / glm_combined$null.deviance), 2)
 
 summary(glm_combined)
+
+# Predict home range radii from mass and hwi
+prediction_data = data.frame(mass = unknown_home_ranges$mass, hand_wing_index = unknown_home_ranges$hand_wing_index)
+predicted_home_range_by_both = predict_with_ci(glm_combined, prediction_data) %>% mutate(
+  mass = prediction_data$mass,
+  hand_wing_index = prediction_data$hand_wing_index,
+  common_name = unknown_home_ranges$common_name
+)
 
 ###################################################################################
 
@@ -128,25 +137,30 @@ summary(glm_combined)$deviance
 glm_mass_pseudoR2
 glm_hwi_pseudoR2
 glm_combined_pseudoR2
+summary(predicted_home_range_by_mass$predicted)
+summary(predicted_home_range_by_hwi$predicted)
+summary(predicted_home_range_by_both$predicted)
 
 ###################################################################################
 
-home_range_sizes = full_join(predicted_home_range_by_mass %>% mutate(home_range_radius_m = predicted), trait_data %>% filter(!is.na(home_range_radius_m)) %>% select(common_name, home_range_radius_m, mass)) %>% arrange(home_range_radius_m)
+trait_data = trait_data %>% left_join(predicted_home_range_by_both %>% select(common_name, predicted, lower, upper), by = "common_name") %>%
+  mutate(home_range_radius_m = ifelse(is.na(home_range_radius_m), predicted, home_range_radius_m))
 
-home_range_sizes$range_bin <- cut(home_range_sizes$home_range_radius_m,
-                    breaks = quantile(home_range_sizes$home_range_radius_m, probs = seq(0, 1, length.out = 5), na.rm = TRUE),
+trait_data$range_bin = cut(trait_data$home_range_radius_m,
+                    breaks = quantile(trait_data$home_range_radius_m, probs = seq(0, 1, length.out = 5), na.rm = TRUE),
                     include.lowest = TRUE,
                     labels = c("Very Small", "Small", "Medium", "Large"))
 
-median_hr = round(median(home_range_sizes$home_range_radius_m), 0)
-mean_hr = round(mean(home_range_sizes$home_range_radius_m), 0)
-max_hr = round(max(home_range_sizes$home_range_radius_m), 0)
-min_hr = round(min(home_range_sizes$home_range_radius_m), 0)
+median_hr = round(median(trait_data$home_range_radius_m), 0)
+mean_hr = round(mean(trait_data$home_range_radius_m), 0)
+max_hr = round(max(trait_data$home_range_radius_m), 0)
+min_hr = round(min(trait_data$home_range_radius_m), 0)
 
-ggplot(home_range_sizes, aes(x = "",y = home_range_radius_m)) +
+ggplot(trait_data, aes(x = "",y = home_range_radius_m)) +
   geom_violin() +
   geom_jitter(width = 0.1, alpha = 0.5, color = "black") +
   scale_y_log10() +
   labs(title = "Distribution of home range radii (m)",
        subtitle = paste0("Median ", median_hr, "\nMean ", mean_hr, "\nMax ", max_hr, "\nMin ", min_hr),
        x = "", y = "Estimated home range radius (m)")
+
