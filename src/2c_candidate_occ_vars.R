ef2f507b0f001c0154d4f63cecb4a979c298d681
Gio Jacuzzi <giojacuzzi@gmail.com>
2025-08-22 10:26:19 -0700

    towards species-specific scales

--- src/2c_candidate_occ_vars.R
+++ src/2c_candidate_occ_vars.R
@@ -1,6 +1,7 @@
 ##############################################################################
 # Finalize candidate set of variables for occurrence
 ##############################################################################
+library(progress)
 library(car)
 library(mapview)
 library(terra)
@@ -19,22 +20,23 @@ path_data_homerange_scale = "data/cache/occurrence_covariates/data_homerange_sca
 message('Loading homerange scale data from cache ', path_data_homerange_scale)
 data_homerange_scale = readRDS(path_data_homerange_scale)
 
-path_rast_cover_clean = "data/cache/occurrence_covariates/rast_cover_clean.tif"
-message('Loading raster cover data from cache ', path_rast_cover_clean)
-rast_cover_clean = rast(path_rast_cover_clean)
-
-mapview(rast_cover_clean,
-        alpha.regions = 1.0,
-        col.regions = c('#90c6bd', '#3c8273', '#d8c18a', '#9b652b', 'darkgray', '#6495ED')) +
-  mapview(data_plot_scale) +
-  mapview(layer.name = 'min', st_buffer(data_plot_scale, unique(data_homerange_scale[['min']]$buffer_radius_m)),
-          alpha.regions = 0.0, lwd = 2) +
-  mapview(layer.name = 'median', st_buffer(data_plot_scale, unique(data_homerange_scale[['median']]$buffer_radius_m)),
-          alpha.regions = 0.0, lwd = 2) +
-  mapview(layer.name = 'mean', st_buffer(data_plot_scale, unique(data_homerange_scale[['mean']]$buffer_radius_m)),
-          alpha.regions = 0.0, lwd = 2) +
-  mapview(layer.name = 'max', st_buffer(data_plot_scale, unique(data_homerange_scale[['max']]$buffer_radius_m)),
-          alpha.regions = 0.0, lwd = 2)
+# path_rast_cover_clean = "data/cache/occurrence_covariates/rast_cover_clean.tif"
+# message('Loading raster cover data from cache ', path_rast_cover_clean)
+# rast_cover_clean = rast(path_rast_cover_clean)
+# 
+# stratum_colors = c('#90c6bd', '#3c8273', '#d8c18a', '#9b652b', '#b2675e', 'darkgray', '#6495ed')
+# mapview(rast_cover_clean,
+#         alpha.regions = 1.0,
+#         col.regions = stratum_colors) +
+#   mapview(data_plot_scale, zcol = 'stratum', col.regions = stratum_colors) +
+#   mapview(layer.name = 'plot', st_buffer(data_plot_scale, unique(data_homerange_scale[['plot']]$buffer_radius_m)),
+#           alpha.regions = 0.0, lwd = 2) +
+#   mapview(layer.name = 'median', st_buffer(data_plot_scale, unique(data_homerange_scale[['median']]$buffer_radius_m)),
+#           alpha.regions = 0.0, lwd = 2) +
+#   mapview(layer.name = 'mean', st_buffer(data_plot_scale, unique(data_homerange_scale[['mean']]$buffer_radius_m)),
+#           alpha.regions = 0.0, lwd = 2) +
+#   mapview(layer.name = 'max', st_buffer(data_plot_scale, unique(data_homerange_scale[['max']]$buffer_radius_m)),
+#           alpha.regions = 0.0, lwd = 2)
 
 data_plot_scale = data_plot_scale %>% st_drop_geometry()
 
@@ -44,7 +46,8 @@ pairwise_collinearity = function(vars, threshold = 0.8) {
   return(collinearity_candidates = subset(as.data.frame(as.table(cor_matrix)), !is.na(Freq) & abs(Freq) >= threshold))
 }
 
-### Collinearity analysis - local plot scale variables from habitat survey ##############################################################
+###########################################################################################
+### Collinearity analysis - local plot scale variables from habitat survey
 message("Assessing collinearity among variables at local plot scale (habitat survey)")
 
 var_candidates_plotscale_hs = data_plot_scale %>% select(where(is.numeric), -stratum)
@@ -57,7 +60,7 @@ var_candidates_plotscale_hs = var_candidates_plotscale_hs %>% select(-all_of(c(
   'plot_tree_gte10cm_density_psme', 'plot_tree_gte10cm_density_thpl', 'plot_tree_gte10cm_density_abam', 'plot_tree_gte10cm_density_tshe', 'plot_tree_gte10cm_density_alru', 'plot_tree_gte10cm_density_pisi',
   'plot_tree_all_density_thpl', 'plot_tree_all_density_abam', 'plot_tree_all_density_tshe', 'plot_tree_all_density_alru', 'plot_tree_all_density_pisi', 'plot_tree_all_density_psme',
   # Drop distance from intermittent streams; favor only consistent watercourses indicative of different riparian habitat.
-  'dist_watercourses_all'
+  'dist_watercourse_all'
 )))
 (names(var_candidates_plotscale_hs))
 pairwise_collinearity(var_candidates_plotscale_hs)
@@ -91,11 +94,11 @@ sort(vif(model))
 # RESULT: Do not include plot_ba_hs alongside plot_qmd_all_hs
 (var_candidates_plotscale_hs = sort(names(sort(vif(model)))))
 
-
-### Collinearity analysis - local plot scale variables from remote sensing ##############################################################
+###########################################################################################
+### Collinearity analysis - local plot scale variables from remote sensing
 message("Assessing collinearity among variables at local plot scale (remote sensing)")
 
-var_candidates_plotscale_rs = data_homerange_scale[['min']] %>% select(where(is.numeric), -buffer_radius_m) # NOTE: 'min' == 100m radius
+var_candidates_plotscale_rs = data_homerange_scale[['plot']] %>% select(where(is.numeric), -buffer_radius_m) # NOTE: 'plot' == 100m radius
 var_candidates_plotscale_rs = var_candidates_plotscale_rs %>% select(starts_with("homerange_"))
 
 # A priori reduce list of candidate plot scale variables (remove irrelevant variables)
@@ -135,8 +138,8 @@ sort(vif(model))
 # Do not include homerange_ba_mean alongside homerange_qmd_mean or homerange_snagden_gt15dbh_mean
 (var_candidates_plotscale_rs = sort(names(sort(vif(model)))))
 
-
-### Collinearity analysis - homerange scale variables (composition, configuration) ############################################
+###########################################################################################
+### Collinearity analysis - homerange scale variables (composition, configuration
 message("Assessing collinearity among variables at homerange scale (composition, configuration)")
 
 scale = 'median' # e.g. 'min', 'median', 'mean', 'max'
@@ -213,8 +216,8 @@ sort(vif(model))
 # density_roads causes multicollinearity at large spatial scales
 (var_candidates_homerangescale = sort(names(sort(vif(model)))))
 
-
-### Collinearity analysis - plot scale (habitat survey) plus homerange scale ##############################################################
+###########################################################################################
+### Collinearity analysis - plot scale (habitat survey) plus homerange scale
 message("Assessing collinearity among remaining variables at both the plot (habitat survey) and homerange (composition, configuration) scales")
 
 scale = 'median' # e.g. 'min', 'median', 'mean', 'max'
@@ -235,12 +238,13 @@ var_candidates_hs_combined = sort(names(sort(vif(model))))
 message("Final list of ", ncol(var_candidates_hs_combined)," uncorrelated variables at both the plot (habitat survey) and homerange (composition, configuration) scales:")
 print(var_candidates_hs_combined)
 
-### Collinearity analysis - plot scale (remote sensing) plus homerange scale ##############################################################
+###########################################################################################
+### Collinearity analysis - plot scale (remote sensing) plus homerange scale
 message("Assessing collinearity among remaining variables at both the plot (remote sensing) and homerange (composition, configuration) scales")
 
-scale = 'median' # e.g. 'min', 'median', 'mean', 'max'
+scale = 'median' # e.g. 'plot', 'median', 'mean', 'max'
 var_candidates_rs_combined = full_join(
-  data_homerange_scale[['min']] %>% select(all_of(c("site", var_candidates_plotscale_rs))), # 100m local plot scale
+  data_homerange_scale[['plot']] %>% select(all_of(c("site", var_candidates_plotscale_rs))), # 100m local plot scale
   data_homerange_scale[[scale]] %>% select(all_of(c("site", var_candidates_homerangescale))),
   by = 'site'
 ) %>% select(-site)
@@ -256,8 +260,6 @@ var_candidates_rs_combined = sort(names(sort(vif(model))))
 message("Final list of ", ncol(var_candidates_hs_combined)," uncorrelated variables at both the plot (remote sensing) and homerange (composition, configuration) scales:")
 print(var_candidates_rs_combined)
 
-
-
 # ### Principal component analysis ##############################################################
 # 
 # var_candidates_scaled = scale(na.omit(var_candidates))
@@ -271,4 +273,244 @@ print(var_candidates_rs_combined)
 # factoextra::fviz_pca_ind(pca, geom.ind = "point", col.ind = "cos2", repel = TRUE)
 # factoextra::fviz_pca_ind(pca, geom.ind = "point", addEllipses = TRUE, legend.title = "Group", repel = TRUE)
 # factoextra::fviz_pca_var(pca, col.var = "contrib", repel = TRUE)
+
+###########################################################################################
+### Collinearity analysis - plot scale (remote sensing) plus species-specific homerange scales
+
+# TODO: pairwise r and VIF for each species, keep track of which variables are correlated
+
+# Start with collinearity among species-specific homerange scales
+names(data_homerange_scale) = tolower(names(data_homerange_scale))
+
+plot_data_homerange_scale = data_homerange_scale[['plot']] # default minimum at 100m
+
+# Remove irrelevant species that are not included in the analysis
+species_to_keep = c(
+  "american crow",
+  "american goldfinch",
+  "american goshawk",
+  "american kestrel",
+  "american robin",
+  "bald eagle",
+  "band-tailed pigeon",
+  "barred owl",
+  "belted kingfisher",
+  "black-headed grosbeak",
+  "black-throated gray warbler",
+  "brown creeper",
+  "brown-headed cowbird",
+  "canada jay",
+  "cedar waxwing",
+  "chestnut-backed chickadee",
+  "common nighthawk",
+  "common raven",
+  "dark-eyed junco",
+  "downy woodpecker",
+  "eurasian collared-dove",
+  "evening grosbeak",
+  "golden-crowned kinglet",
+  "hairy woodpecker",
+  "hammond's flycatcher",
+  "hermit thrush",
+  "hutton's vireo",
+  "macgillivray's warbler",
+  "marbled murrelet",
+  "northern flicker",
+  "northern pygmy-owl",
+  "northern saw-whet owl",
+  "olive-sided flycatcher",
+  "orange-crowned warbler",
+  "pacific wren",
+  "pileated woodpecker",
+  "pine siskin",
+  "purple finch",
+  "red crossbill",
+  "red-breasted nuthatch",
+  "red-tailed hawk",
+  "ruby-crowned kinglet",
+  "ruffed grouse",
+  "rufous hummingbird",
+  "sharp-shinned hawk",
+  "song sparrow",
+  "sooty grouse",
+  "spotted towhee",
+  "steller's jay",
+  "swainson's thrush",
+  "townsend's warbler",
+  "varied thrush",
+  "vaux's swift",
+  "violet-green swallow",
+  "warbling vireo",
+  "western screech-owl",
+  "western tanager",
+  "western wood-pewee",
+  "white-crowned sparrow",
+  "wilson's warbler",
+  "yellow warbler",
+  "yellow-rumped warbler"
+)
+data_homerange_scale = data_homerange_scale[species_to_keep]
+n_scales = length(names(data_homerange_scale))
+
+# A priori remove some vars
+candidates = c(
+  "aggregation_idx",
+  # "cover_diversity", # Drop cover diversity metrics for forest cover diversity metrics
+  # "cover_evenness",
+  # "cover_richness",
+  "cover_forest_diversity",
+  "cover_forest_evenness",
+  # "cover_forest_richness", # No variance at highest spatial scales           
+  "density_edge_cw",
+  "density_roads",                     
+  "density_roads_paved",
+  # "density_streams", # Drop density streams for major streams (which are perennial and bordered by riparian habitat)
+  "density_streams_major",             
+  "focalpatch_area_homeange_pcnt",  
+  "focalpatch_core_area_homeange_pcnt",
+  # "focalpatch_age_cv", # Only look at landscape variables for now
+  # "focalpatch_age_mean",
+  # "focalpatch_ba_cv",
+  # "focalpatch_ba_mean",
+  # "focalpatch_canopy_closure_cv",      
+  # "focalpatch_canopy_closure_mean",
+  # "focalpatch_canopy_cover_cv",
+  # "focalpatch_canopy_cover_mean",      
+  # "focalpatch_canopy_layers_cv",
+  # "focalpatch_canopy_layers_mean",
+  # "focalpatch_downvol_cv",
+  # "focalpatch_downvol_mean",
+  # "focalpatch_htmax_cv",               
+  # "focalpatch_htmax_mean",
+  # "focalpatch_isolation",
+  # "focalpatch_qmd_cv",                 
+  # "focalpatch_qmd_mean",
+  # "focalpatch_snagden_gt15dbh_cv",
+  # "focalpatch_snagden_gt15dbh_mean",   
+  # "focalpatch_treeden_all_cv",
+  # "focalpatch_treeden_all_mean",
+  # "focalpatch_treeden_gt4in_dbh_cv",   
+  # "focalpatch_treeden_gt4in_dbh_mean",
+  # "homerange_age_cv",
+  # "homerange_age_mean",                
+  # "homerange_ba_cv",
+  # "homerange_ba_mean",
+  # "homerange_canopy_closure_cv",       
+  # "homerange_canopy_closure_mean",
+  # "homerange_canopy_cover_cv",
+  # "homerange_canopy_cover_mean",       
+  # "homerange_canopy_layers_cv",
+  # "homerange_canopy_layers_mean",
+  # "homerange_downvol_cv",              
+  # "homerange_downvol_mean",
+  # "homerange_htmax_cv",
+  # "homerange_htmax_mean",              
+  # "homerange_qmd_cv",
+  # "homerange_qmd_mean",
+  # "homerange_snagden_gt15dbh_cv",      
+  # "homerange_snagden_gt15dbh_mean",
+  # "homerange_treeden_all_cv",
+  # "homerange_treeden_all_mean",        
+  # "homerange_treeden_gt4in_dbh_cv",
+  # "homerange_treeden_gt4in_dbh_mean",
+  "prop_abund_comthin",                
+  "prop_abund_lsog",
+  "prop_abund_oldgrowth",
+  "prop_abund_roads",             
+  "prop_abund_standinit",
+  "prop_abund_stemexcl",
+  "prop_abund_undstryreinit",     
+  "prop_abund_water",
+  "shape_idx"
+)
+
+all_species_results = data.frame()
+for (i in 1:n_scales) {
+  scale = names(data_homerange_scale)[i]
+  print(scale)
+  species_homerange_data = data_homerange_scale[[scale]]
+  buffer_radius_m = unique(species_homerange_data$buffer_radius_m)
+  # if buffer_radius_m < 100m, set to 100m and pull data from "plot" scale
+  if (buffer_radius_m < 100) {
+    species_homerange_data = plot_data_homerange_scale
+  }
+  species_var_candidates_homerange = species_homerange_data %>% select(where(is.numeric), -buffer_radius_m)
+  
+  # A priori reduce list of candidate plot scale variables (remove irrelevant variables)
+  species_var_candidates_homerange = species_var_candidates_homerange %>% select(all_of(candidates))
+  (names(species_var_candidates_homerange))
+  
+  sd0 = names(species_var_candidates_homerange)[apply(species_var_candidates_homerange, 2, sd, na.rm = TRUE) == 0]
+  if (length(sd0) > 0) {
+    message("Species ", scale, " has zero-variance variables: ", paste(vars_sd0, collapse = ", "))
+  }
+  
+  species_results = pairwise_collinearity(species_var_candidates_homerange) %>% mutate(scale = scale)
+  all_species_results = rbind(all_species_results, species_results)
+}
+
+# Make a consistent pair ID with a unique separator
+all_species_results$pair = apply(
+  all_species_results[,c("Var1","Var2")], 1,
+  function(x) paste(sort(x), collapse = "#")
+)
+# Count number of scales where each pair is flagged
+pair_summary = aggregate(scale ~ pair, data = all_species_results, FUN = function(x) length(unique(x)))
+pair_summary$var1 = sub("#.*", "", pair_summary$pair)
+pair_summary$var2 = sub(".*#", "", pair_summary$pair)
+pair_summary = pair_summary[,c("var1","var2","scale")]
+pair_summary$pcnt_species = round(pair_summary$scale / length(unique(all_species_results$scale)), 2)
+pair_summary = pair_summary %>% arrange(desc(pcnt_species)) %>% rename(freq = scale)
+print(pair_summary)
+
+# Reduce list of candidate variables (highly correlated, less preferable, etc.)
+candidates = candidates[!candidates %in% c(
+  # The following variables cause issues of collinearity across many homerange scales
+  'cover_forest_evenness', # ~ cover_forest_diversity
+  'prop_abund_roads',                               # ~ density_roads_paved
+  'prop_abund_water',                               # ~ density_streams_major
+  'focalpatch_core_area_homeange_pcnt', # ~ focalpatch_area_homeange_pcnt, aggregation_idx, cover_forest_diversity
+  'aggregation_idx', # ~ focalpatch_area_homeange_pcnt, focalpatch_core_area_homeange_pcnt, density_edge_cw
+  'prop_abund_undstryreinit', 'prop_abund_oldgrowth', # prop_abund_lsog = prop_abund_undstryreinit + prop_abund_oldgrowth
+  'prop_abund_standinit',  # ~ density_edge_cw
+  'prop_abund_stemexcl' # ~ cover_forest_diversity
+)]
+
+
+# VIF analysis
+all_species_vif_results = data.frame()
+for (i in 1:n_scales) {
+  scale = names(data_homerange_scale)[i]
+  print(scale)
+  species_homerange_data = data_homerange_scale[[scale]]
+  buffer_radius_m = unique(species_homerange_data$buffer_radius_m)
+  # if buffer_radius_m < 100m, set to 100m and pull data from "plot" scale
+  if (buffer_radius_m < 100) {
+    species_homerange_data = plot_data_homerange_scale
+  }
+  # Discard non-candidate variables
+  species_homerange_data = species_homerange_data[, names(species_homerange_data) %in% candidates, drop = FALSE]
+
+  model = lm(rep(1, nrow(species_homerange_data)) ~ ., data = species_homerange_data)
+  results = as.data.frame(t(vif(model)))
+  results = results %>% mutate (scale = scale)
+  all_species_vif_results = rbind(all_species_vif_results, results)
+}
+mean_vif = apply(all_species_vif_results[, -ncol(all_species_vif_results)], 2, mean)
+print(sort(mean_vif, decreasing = TRUE))
+
+# Count frequency of VIF >= 10 for each var across species scales
+vif_values = all_species_vif_results %>% select(-scale)
+freq = colSums(vif_values >= 10)
+vif_summary = data.frame(
+  var  = names(freq),
+  freq = freq,
+  pcnt_species = round( freq / nrow(vif_values), 2),
+  row.names = NULL
+)
+vif_summary = vif_summary %>% arrange(desc(pcnt_species))
+print(vif_summary)
+
+
+# TODO: Introduce plot scale (remote sensing) variables
 
